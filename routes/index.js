2ac077f788faadaa009c8f4a9a81849207731c75
Jerr-z <zmx201518@gmail.com>
2024-11-01 19:36:09 -0700

    add call without arguments

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
-var express = require('express');
+import express from 'express'
+import puppeteerCluster from '../helpers/ConfiguredCluster';
+
+
 var router = express.Router();
+let clusterInstance = puppeteerCluster.getInstance().cluster
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/court1', function(req, res) {
-  await //
+router.get('/court', function(req, res) {
+  // TODO validate data
+  clusterInstance.queue();
 });
 module.exports = router;

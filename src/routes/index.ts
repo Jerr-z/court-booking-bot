7604cbe8a86c5c9b968f0e15183d58cc7654727f
Jerr-z <zmx201518@gmail.com>
2024-12-19 00:00:45 -0800

    Added validation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import puppeteerCluster from '../helpers/ConfiguredCluster.js';
 import { bookUBCCourtsTask, getAllCourtUrlsTask } from '../tasks/ubc-tennis-task.js';
+import {BookUBCTaskRequestBodySchema} from "../model/schema.js";
+import {calculateSleepTime} from "../helpers/utils.js";
+import { Cluster } from 'puppeteer-cluster';
 
 
-var router = express.Router();
-let clusterInstance;
+const router = express.Router();
+let clusterInstance: Cluster<any, any>;
 (async () => {
     clusterInstance = await puppeteerCluster.getInstance();
 })();
@@ -16,9 +19,18 @@ router.get('/', function(req, res, next) {
 
 router.get('/court', function(req, res) {
   // TODO validate data
-  console.log(req.body)
-  clusterInstance.execute(req.body, bookUBCCourtsTask);
-  console.log('Task queued');
+
+  let validationResult = BookUBCTaskRequestBodySchema.validate(req.body)
+
+  if (validationResult.error) {
+    res.status(400).send(`Error: ${validationResult.error}`);
+    return;
+  }
+
+  console.log(validationResult.value)
+  let triggerDelay = calculateSleepTime(new Date(validationResult.value.startTime))
+  setTimeout(() => clusterInstance.execute(validationResult.value, bookUBCCourtsTask), triggerDelay)
+  console.log(`Task queued, waking up in ${triggerDelay} ms`);
   res.status(200).send();
 });
 

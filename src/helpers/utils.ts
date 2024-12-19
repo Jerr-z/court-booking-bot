7ca4ed82d0b0d91c9cb73f8d87b39c45680f67bb
Jerr-z <zmx201518@gmail.com>
2024-12-19 00:02:39 -0800

    Added function for calculating delay

---
+++ src/helpers/utils.ts
@@ -0,0 +1,21 @@
+export function calculateSleepTime(until: Date) {
+    const targetHour = until.getHours() - 1;
+    const targetMinute = 59;
+    const targetSecond = 59;
+
+    const now = new Date()
+    const currentHour = now.getHours();
+    const currentMinute = now.getMinutes();
+    const currentSecond = now.getSeconds();
+
+    const targetTimeMs =
+        targetHour * 3600000 + targetMinute * 60000 + targetSecond * 1000;
+    const currentTimeMs =
+        currentHour * 3600000 + currentMinute * 60000 + currentSecond * 1000;
+
+    let difference = targetTimeMs - currentTimeMs;
+    if (difference <= 0) {
+        difference += 86400000
+    }
+    return difference;
+}
